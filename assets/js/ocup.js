a4bd959026a43ec8a94c586c5837651896276c15
timercs <162223868+timercs@users.noreply.github.com>
2024-10-21 14:52:51 -0300

    Update ocup.js

--- assets/js/ocup.js
+++ assets/js/ocup.js
@@ -100,7 +100,7 @@ function searchAddress() {
 //### Função que converte csv para um kml com seus poligonos
 function convertCSVtoLeaflet() {
     const local = searchUF.value
-    const url = 'https://gitlab.com/devppnacional/csv/-/raw/main/' + local + '.csv'
+    const url = 'https://gitlab.com/caveirastodosportodos/csv/-/raw/main/' + local + '.csv'
 
     if (searchUF.value != 'UF') {
         //### Liberar Botão de Download
